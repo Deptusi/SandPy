17dba9806d080e64ac7a805e9bf1b4389c8d0537
Elrequena <Elrequena@users.noreply.github.com>
2021-05-21 01:02:26 -0400

    cambio en variables (sin caracteres especiales), cambio de listas por tuplas en diccionarios

--- Sand_classes.py
+++ Sand_classes.py
@@ -1,78 +1,71 @@
-# -*- coding: cp1252 -*-
-
 class Sandwich():
 
-    def __init__(self,número_de_sandwich=1):
-        self.número_de_sandwich=número_de_sandwich
-        self.precio=0
-        self.lista_de_tamaños={
-            "t":[580,"Triple"],
-            "d":[430, "Doble"],
-            "i":[280, "Individual"],
+    def __init__(self,numero_de_sandwich=1):
+        self.numero_de_sandwich = numero_de_sandwich
+        self.precio = 0
+        self.lista_de_tamanos={
+            "t":(580,"Triple"),
+            "d":(430, "Doble"),
+            "i":(280, "Individual"),
         }
-        self.tamaño=""
+        self.tamano=""
         self.lista_de_ingredientes={
-            "ja":[40, "Jamón"],
-            "ch":[35, "Champiñones"],
-            "pi":[30, "Pimentón"],
-            "dq":[40, "Doble Queso"],
-            "ac":[57.5, "Aceitunas"],
-            "pp":[38.5, "Pepperoni"],
-            "sa":[62.5, "Salchichón"]
+            "ja":(40, "JamÃ³n"),
+            "ch":(35, "ChampiÃ±ones"),
+            "pi":(30, "PimentÃ³n"),
+            "dq":(40, "Doble Queso"),
+            "ac":(57.5, "Aceitunas"),
+            "pp":(38.5, "Pepperoni"),
+            "sa":(62.5, "SalchichÃ³n")
         }
         self.sandwich_terminado=False # Variable Boolean que indica cuando se terminan 
         # de agregar ingredientes
-        self.lista_descriptiva=[] # Variable que almacena el tamaño e ingredientes del Sandwich
+        self.lista_descriptiva=[] # Variable que almacena el tamaï¿½o e ingredientes del Sandwich
     
-    def definir_tamaño(self):
+    def definir_tamano(self):
         print("Opciones:")
         while True:
-            opción_seleccionada=input("Tamaños: Triple ( t ) Doble ( d ) Individual ( i ): ")
-            if opción_seleccionada in self.lista_de_tamaños:
+            opcion_seleccionada=input("TamaÃ±os: Triple ( t ) Doble ( d ) Individual ( i ): ")
+            if opcion_seleccionada in self.lista_de_tamanos:
                 break
             else:
-                print("=> Debe seleccionar el tamaño correcto!!")
-        self.tamaño=(self.lista_de_tamaños[opción_seleccionada])
+                print("=> Debe seleccionar el tamaÃ±o correcto!!")
+        self.tamano=(self.lista_de_tamanos[opcion_seleccionada])
 
     def agregar_ingrediente(self):
         while True:
 
-            if len(self.lista_de_ingredientes)>0: # si no hay más ingredientes, no tiene sentido continuar
-                opción_seleccionada=input("Indique ingrediente (enter para terminar): ")
+            if len(self.lista_de_ingredientes)>0: # si no hay mÃ¡s ingredientes, no tiene sentido continuar
+                opcion_seleccionada=input("Indique ingrediente (enter para terminar): ")
             else: 
                 self.sandwich_terminado=True
                 break
 
-            if opción_seleccionada in self.lista_de_ingredientes:
+            if opcion_seleccionada in self.lista_de_ingredientes:
                 # Al agregar un ingrediente, utilizo pop para que ya no se pueda agregar
                 self.lista_descriptiva.append(
-                    self.lista_de_ingredientes.pop(opción_seleccionada)
+                    self.lista_de_ingredientes.pop(opcion_seleccionada)
                 )
                 break
-            elif opción_seleccionada=="":
+            elif opcion_seleccionada=="":
                 self.sandwich_terminado=True
                 break
             else:
                 print("=> Debe seleccionar un ingrediente de la lista!!")
 
     def actualizar_precio(self):
-        self.precio=self.tamaño[0]
+        self.precio=self.tamano[0]
         for ingrediente in self.lista_descriptiva:
             self.precio+=ingrediente[0]
 
-    def imprimir_selección(self):
+    def imprimir_seleccion(self):
         self.actualizar_precio()
-        txt=f"Usted seleccionó un sándwich {self.tamaño[1]} con "
+        txt=f"Usted seleccionÃ³ un sÃ¡ndwich {self.tamano[1]} con "
         if len(self.lista_descriptiva)>0:
             for ingrediente in self.lista_descriptiva:
                 txt=txt+ingrediente[1]+", "
-            txt=txt[:-2] # Elimina la última ", "
+            txt=txt[:-2] # Elimina la Ãºltima ", "
         else:
             txt=txt+"Queso"
-        print(txt)
-        print("")
-        print(f"Subtotal a pagar por un sándwich {self.tamaño[1]}: {self.precio}")
-
-
-
-
+        print(txt+"\n")
+        print(f"Subtotal a pagar por un sÃ¡ndwich {self.tamano[1]}: {self.precio}")
--- Sand_functions.py
+++ Sand_functions.py
@@ -1,23 +1,22 @@
-# -*- coding: cp1252 -*-
 from Sand_classes import *
 
 def print_Tittle():
-    print("*"*26)
-    print("*     SANDWICHES UCAB    *")
-    print("*"*26)
+    print("*"*27)
+    print("*     SANDWICHES UCAB     *")
+    print("*"*27)
 
-def ordenar_sándwich(orden_N):
-    print(f"Sandwich número {orden_N}")
+def ordenar_sandwich(orden_N):
+    print(f"Sandwich nÃºmero {orden_N}")
     Sandwich_temporal=Sandwich()
-    Sandwich_temporal.definir_tamaño()
+    Sandwich_temporal.definir_tamano()
     imprimir_ingredientes(Sandwich_temporal)
     while not Sandwich_temporal.sandwich_terminado:
         Sandwich_temporal.agregar_ingrediente()
-    Sandwich_temporal.imprimir_selección()
+    Sandwich_temporal.imprimir_seleccion()
     return Sandwich_temporal
 
 def imprimir_ingredientes(Sandwich):
-    # Aquí no encuentro la forma para que quede alineada la leyenda
+    # AquÃ­ no encuentro la forma para que quede alineada la leyenda
     print("Ingredientes" ,"\n")
     for ingrediente in Sandwich.lista_de_ingredientes:
         print(
@@ -26,30 +25,30 @@ def imprimir_ingredientes(Sandwich):
 
 def input_continue():
     print("*"*28,)
-    temp=input("¿Desea continuar [s/n]?: ")
+    temp=input("Â¿Desea continuar [s/n]?: ")
     while temp!="s" and temp!="n":
-        temp=input("¿Desea continuar [s/n]?: ")
+        temp=input("Â¿Desea continuar [s/n]?: ")
     if temp=="s":
         return True
     else:
         return False
 
 def realizar_ordenes():
-    número_de_orden=1
-    lista_de_ordenes=[]
-    lista_de_ordenes.append(ordenar_sándwich(número_de_orden))
+    numero_de_orden = 1
+    lista_de_ordenes = []
+    lista_de_ordenes.append(ordenar_sandwich(numero_de_orden))
     while input_continue():
-        número_de_orden+=1
-        lista_de_ordenes.append(ordenar_sándwich(número_de_orden))
+        numero_de_orden+=1
+        lista_de_ordenes.append(ordenar_sandwich(numero_de_orden))
     
     return lista_de_ordenes
 
 def imprimit_total(lista_de_ordenes):
     print("*"*28,)
     print(f"El pedido tiene un total de {len(lista_de_ordenes)} \
-sándwich(es) por un monto de {sum([sandwich.precio for sandwich in lista_de_ordenes])}")
-    print("")
-    # aquí podríamos agregar una opción para dar un resumen detallado de los 
+sÃ¡ndwich(es) por un monto de {sum([sandwich.precio for sandwich in lista_de_ordenes])}")
+    
+    # aquÃ­ podrÃ©amos agregar una opciÃ³n para dar un resumen detallado de los 
     # pedidos, y preguntar si quiere eliminar un pedido, o duplicar otro
-    print("Gracias por su compra, regrese pronto")
+    print("\nGracias por su compra, regrese pronto")
 
